8086edae9a4d34fc1cf10d941f5b4d5550074bd6
Carlosgit config --global user.name Carlos <CarlosS-P79@hotmail.comgit config --global user.email CarlosS-P79@hotmail.com>
2020-11-18 10:25:15 +0100

    H

---
+++ ej362eldiadenavidad.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class ej362eldiadenavidad {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+int dia , mes;
+		
+		
+		
+while () {
+	
+	if () {
+		
+		
+	}
+	if () {
+		
+		
+		
+		
+	}
+		
+	System.out.println();
+	
+		
+	
+}
+
+
+	}
+}
